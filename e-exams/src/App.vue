b4d5fa65fde135048b7b23b2aa62da4d14a5556c
Andew Mwale <mwaleandrew99@gmail.com>
2023-12-19 10:09:49 +0000

    first commit

---
+++ e-exams/src/App.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HelloWorld from './components/LandingPage.vue'
+</script>
+
+<template><div>
+  <header>
+    <div class="wrapper">
+      
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView /> 
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  height: 10vh;
+  border: 1px solid;
+  width: 500%;
+  background: #ddd0dd;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+  padding: 8px;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+  background: transparent;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  /* .logo {
+    margin: 0 2rem 0 0;
+  } */
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ e-exams/src/components/LandingPage.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+<h1>E-EXAMS</h1>
+</template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@500&family=Poppins:wght@100&family=Roboto:wght@500&family=Rubik:wght@300&display=swap');
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+</style>
